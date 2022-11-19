df0f2aaa00e6201042e5beb783641a12da316939
dhouse1UNCC <danhouse98@gmail.com>
2022-11-19 07:59:08 -0500

    MVP for testing
    MVP uploaded to github for testing of the ajax feature

---
+++ scripts/items.js
@@ -0,0 +1,88 @@
+/*
+id - holds item id
+int, str, agi, mastery, verse, crit, haste- shows 0 if stat is not present, 1 if stat is present on item
+slot - holds slot the item is equiped to
+type - holds tag for item
+    armor - armor type (ie plate, cloth, leather)
+    trinket - type of trinket
+    weapon - type of weapon w/ stat modifier if applicable
+*/
+//var [] Items;
+
+function Item() {
+    this.id = 0;
+    this.int = 0;
+    this.str = 0;
+    this.agi = 0;
+    this.mastery = 0;
+    this.verse = 0;
+    this.crit = 0;
+    this.haste = 0;
+    this.slot = "";
+    this.type = "";
+}
+
+$(document).ready(function () {
+    var slots = ['head', 'shoulder', 'chest', 'wrist', 'hands', 'waist', 'legs', 'feet', 'neck', 'back', 'ring', 'weapon', 'trinket'];
+
+    $.ajax({
+        type: "get",
+        url: "json/items.json",
+        beforeSend: function () {
+            //TODO rename id tag
+            //$("#comparisontext").html("Loading...");
+        },
+        timeout: 10000,
+        error: function (xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function (data) {
+            //TODO rename id tag
+            $("dungeonList").html("");
+            //TODO rename id tag
+            $.each(data, function () {
+                $.each(this, function (key, value) {
+
+                    var dungeonID = "";
+                    var bossID = "";
+                    var bossSlotID = "";
+
+                    for (var i = 0; i < data.dungeon.length; i++) {
+                        //sets dungeon id respective to amount of dungeons
+                        dungeonID = i.toString();
+                        for (var j = 0; j < data.dungeon[i].bosses.length; j++) {
+                            //sets dungeon id respective to amount of bosses
+                            bossID = j.toString();
+                            var bossTableHTML = "<div class= 'dungeon'><div class=''boss'><table><tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
+
+                            for (var k = 0; k < slots.length; k++) {
+                                bossSlotID += dungeonID + "." + bossID + "." + slots[k];
+                                bossTableHTML += "<tr>" + bossSlotID + '</tr>';
+                            }
+
+                            bossTableHTML += "</tr></table></div></div>";
+                            $('#dungeonList').append(bossTableHTML);
+
+                            for (var k = 0; k < data.dungeon[i].bosses[j].drops.length; k++) {
+
+                                bossSlotID = "#" + dungeonID + "." + bossID + "." + data.dungeon[i].bosses[j].drops[k].slot;
+                                //build wowhead links
+                                //TODO implement dynamic id values to append to the correct td element
+                                $(bossSlotID).append(
+                                    '<a id=' + data.dungeon[i].bosses[j].drops[k].id + ' href="https://www.wowhead.com/item="' + data.dungeon[i].bosses[j].drops[k].id +
+                                    '" data-wh-icon-added="true" class="q3" data-wh-rename-link="false">' +
+                                    '<span class="iconmedium" data-env="live" data-tree="live" data-game="wow" data-type="item">' +
+                                    '<ins style="background-image: url(&quot;' + data.dungeon[i].bosses[j].drops[k].imglink + '&quot;);"></ins><del></del></span></a>'
+                                );
+                            }
+                        }
+                    }
+                });
+            });
+        },
+        //On completeion, show the entry with index 0
+        complete: function () {
+        }
+    });
+});
