edcb171592fd6d32e34831d25b2d89afd95a874b
dhouse1UNCC <danhouse98@gmail.com>
2022-11-19 15:24:30 -0500

    AJAX implemented
    Working AJAX code implemented

--- scripts/items.js
+++ scripts/items.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
                     $.each(dungeonList.bosses, function (key2, bossList) {
                         bossID = bossCounter.toString();
 
-                        bossTableHTML = "<div class='boss'><table><caption>" + bossList.name + "</caption><tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
+                        bossTableHTML = "<div class='boss'><img src='https://wow.zaming.com/images/wow/journal/" + bossList.imglink + "' alt='"+ bossList.name + "'><table><caption>" + bossList.name + "</caption><tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
 
                         for (var k = 0; k < slots.length; k++) {
                             bossSlotID = dungeonID + "." + bossID + "." + slots[k];
