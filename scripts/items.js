39bf3b320b1f515ad1df6d5c15eae5ae6aa7da14
dhouse1UNCC <danhouse98@gmail.com>
2022-11-19 19:09:29 -0500

    Controller Progress
    Added Slots and Class filter controls

--- scripts/items.js
+++ scripts/items.js
@@ -22,8 +22,10 @@ function Item() {
     this.type = "";
 }
 
+var slots = ['head', 'shoulder', 'chest', 'wrist', 'hands', 'waist', 'legs', 'feet', 'neck', 'back', 'ring', 'weapon', 'trinket'];
+
+
 $(document).ready(function () {
-    var slots = ['head', 'shoulder', 'chest', 'wrist', 'hands', 'waist', 'legs', 'feet', 'neck', 'back', 'ring', 'weapon', 'trinket'];
 
     $.ajax({
         type: "get",
@@ -48,7 +50,7 @@ $(document).ready(function () {
                 var bossID = "";
                 var bossSlotID = "";
                 //dungeonList
-                $.each(this, function(key, dungeonList){
+                $.each(this, function (key, dungeonList) {
                     var bossCounter = 0;
                     dungeonID = dungeonCounter.toString();
                     var bossTableHTML = "<div class= 'dungeon' id= 'dungeon" + dungeonID + "'><h2>" + dungeonList.dungeon_name + "</h2>";
@@ -58,90 +60,193 @@ $(document).ready(function () {
                     $.each(dungeonList.bosses, function (key2, bossList) {
                         bossID = bossCounter.toString();
 
-                        bossTableHTML = "<div class='boss'><div class='imgbox'><img src='https://wow.zamimg.com/images/wow/journal/" + bossList.imglink + "' alt='"+ bossList.name + "'><h3>" + bossList.name + "</h3></div><table>" + 
-                        "<tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
+                        bossTableHTML = "<div class='boss'><div class='imgbox'><img src='https://wow.zamimg.com/images/wow/journal/" + bossList.imglink + "' alt='" + bossList.name + "'><h3>" + bossList.name + "</h3></div><table>" +
+                            "<tr><th class ='head'>Head</th><th class='shoulder'>Shoulder</th><th class='chest'>Chest</th><th class='wrist'>Wrist</th><th class='hands'>Hands</th>" +
+                            "<th class='waist'>Waist</th><th class='legs'>Legs</th><th class='feet'>Feet</th><th class='neck'>Neck</th><th class='back'>Back</th>" +
+                            "<th class='neck'>Rings</th><th class='weapon'>Weapon</th><th class='trinket'>Trinket</th></tr><tr>";
 
                         for (var k = 0; k < slots.length; k++) {
                             bossSlotID = dungeonID + "." + bossID + "." + slots[k];
-                            bossTableHTML += "<td id='" + bossSlotID + "'></td>";
-                        }   
+                            bossTableHTML += "<td id='" + bossSlotID + "' class='" + slots[k] + "'></td>";
+                        }
 
                         bossTableHTML += "</tr></table></div><br>";
                         $("#dungeonList").append(bossTableHTML);
 
                         //item List
                         $.each(bossList.drops, function (key3, itemList) {
-                            
+
                             bossSlotID = dungeonID + "." + bossID + "." + itemList.slot;
-                            
+
                             var elem = document.getElementById(bossSlotID);
 
                             var itemInfo = '<a id="' + itemList.id + '" href="https://www.wowhead.com/item=' + itemList.id +
-                            ' data-wh-icon-added="true" class="q3" data-wh-rename-link="false">' +
-                            '<span class="iconmedium" data-env="live" data-tree="live" data-game="wow" data-type="item">' +
-                            '<ins style="background-image: url(&quot;https://wow.zamimg.com/images/wow/icons/medium/' + itemList.imglink + '&quot");"></ins><del></del></span></a>';
+                                ' data-wh-icon-added="true" class="q3" data-wh-rename-link="false">' +
+                                '<span class="iconmedium" data-env="live" data-tree="live" data-game="wow" data-type="item">' +
+                                '<ins style="background-image: url(&quot;https://wow.zamimg.com/images/wow/icons/medium/' + itemList.imglink + '&quot");"></ins><del></del></span></a>';
 
-                            if(elem != null ){
+                            if (elem != null) {
                                 elem.insertAdjacentHTML('beforeend', itemInfo);
-                            }else{
+                            } else {
                                 console.log("Invalid entry - ID : " + itemList.id);
                             }
-                            
-                            
+
+
                         });
 
                         bossCounter++;
                     });
-                    
+
                     bossTableHTML = "</div>";
                     $("#dungeonList").append(bossTableHTML);
                     dungeonCounter++;
-                });  
+                });
             });
         }
     });
 });
 
-                    /*
-                    //dungeon list
-                    for (var i = 0; i < data.dungeon.length; i++) {
-                        //sets dungeon id respective to amount of dungeons
-                        dungeonID = i.toString();
+/*
+//dungeon list
+for (var i = 0; i < data.dungeon.length; i++) {
+    //sets dungeon id respective to amount of dungeons
+    dungeonID = i.toString();
 
-                        //boss list
-                        for (var j = 0; j < data.dungeon[i].bosses.length; j++) {
-                            //sets dungeon id respective to amount of bosses
-                            bossID = j.toString();
-                            var bossTableHTML = "<div class= 'dungeon' id= 'dungeon" + i.toString() + "'><div class='boss'><table><tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
+    //boss list
+    for (var j = 0; j < data.dungeon[i].bosses.length; j++) {
+        //sets dungeon id respective to amount of bosses
+        bossID = j.toString();
+        var bossTableHTML = "<div class= 'dungeon' id= 'dungeon" + i.toString() + "'><div class='boss'><table><tr><th>Head</th><th>Shoulder</th><th>Chest</th><th>Wrist</th><th>Hands</th><th>Waist</th><th>Legs</th><th>Feet</th><th>Neck</th><th>Back</th><th>Rings</th><th>Weapon</th><th>Trinket</th></tr><tr>";
 
-                            for (var k = 0; k < slots.length; k++) {
-                                bossSlotID = dungeonID + "." + bossID + "." + slots[k];
-                                bossTableHTML += "<tr id='" + bossSlotID + "'></tr>";
-                            }
+        for (var k = 0; k < slots.length; k++) {
+            bossSlotID = dungeonID + "." + bossID + "." + slots[k];
+            bossTableHTML += "<tr id='" + bossSlotID + "'></tr>";
+        }
 
-                            bossTableHTML += "</tr></table></div></div>";
-                            $('#dungeonList').append(bossTableHTML);
+        bossTableHTML += "</tr></table></div></div>";
+        $('#dungeonList').append(bossTableHTML);
 
-                            //item list
-                            for (var k = 0; k < data.dungeon[i].bosses[j].drops.length; k++) {
+        //item list
+        for (var k = 0; k < data.dungeon[i].bosses[j].drops.length; k++) {
 
-                                bossSlotID = "#" + dungeonID + "." + bossID + "." + data.dungeon[i].bosses[j].drops[k].slot;
-                                //build wowhead links
-                                //TODO implement dynamic id values to append to the correct td element
-                                $(bossSlotID).append(
-                                    '<a id="' + data.dungeon[i].bosses[j].drops[k].id + '" href="https://www.wowhead.com/item=' + data.dungeon[i].bosses[j].drops[k].id +
-                                    ' data-wh-icon-added="true" class="q3" data-wh-rename-link="false">' +
-                                    '<span class="iconmedium" data-env="live" data-tree="live" data-game="wow" data-type="item">' +
-                                    '<ins style="background-image: url(&quot;' + data.dungeon[i].bosses[j].drops[k].imglink + '&quot;);"></ins><del></del></span></a>'
-                                );
-                            }
-                        }
-                    }
-                });
-            });
-        },
-        //On completeion, show the entry with index 0
-        complete: function () {
+            bossSlotID = "#" + dungeonID + "." + bossID + "." + data.dungeon[i].bosses[j].drops[k].slot;
+            //build wowhead links
+            //TODO implement dynamic id values to append to the correct td element
+            $(bossSlotID).append(
+                '<a id="' + data.dungeon[i].bosses[j].drops[k].id + '" href="https://www.wowhead.com/item=' + data.dungeon[i].bosses[j].drops[k].id +
+                ' data-wh-icon-added="true" class="q3" data-wh-rename-link="false">' +
+                '<span class="iconmedium" data-env="live" data-tree="live" data-game="wow" data-type="item">' +
+                '<ins style="background-image: url(&quot;' + data.dungeon[i].bosses[j].drops[k].imglink + '&quot;);"></ins><del></del></span></a>'
+            );
         }
     }
+}
+});
+});
+},
+//On completeion, show the entry with index 0
+complete: function () {
+}
+}
 });*/
+
+
+var classes = {
+    "classList": [{ "class": "Shaman", "subclass": [{ "name": "Elemental" }, { "name": "Enhancement" }, { "name": "Restoration" }] },
+    { "class": "Death Knight", "subclass": [{ "name": "Blood" }, { "name": "Frost" }, { "name": "Unholy" }] }, { "class": "Monk", "subclass": [{ "name": "Mistweaver" }, { "name": "Brewmaster" }, { "name": "Windwalker" }] },
+    { "class": "Demon Hunter", "subclass": [{ "name": "Havoc" }, { "name": "Vengence" }] },
+    { "class": "Warrior", "subclass": [{ "name": "Protection" }, { "name": "Arms" }, { "name": "Fury" }] },
+    { "class": "Mage", "subclass": [{ "name": "Frost", "name": "Fire" }, { "name": "Arcane" }] },
+    { "class": "Hunter", "subclass": [{ "name": "Survival" }, { "name": "Marksmanship" }, { "name": "Beast Mastery" }] },
+    { "class": "Druid", "subclass": [{ "name": "Balance" }, { "name": "Feral" }, { "name": "Guardian" }, { "name": "Restoration" }] },
+    { "class": "Rouge", "subclass": [{ "name": "Outlaw" }, { "name": "Assassination" }, { "name": "Subtlety" }] },
+    { "class": "Priest", "subclass": [{ "name": "Discipline" }, { "name": "Holy" }, { "name": "Shadow" }] },
+    { "class": "Paladin", "subclass": [{ "name": "Holy" }, { "name": "Protection" }, { "name": "Retribution" }] },
+    { "class": "Warlock", "subclass": [{ "name": "Affliction" }, { "name": "Demonology" }, { "name": "Destruction" }] },
+    { "class": "Evoker", "subclass": [{ "name": "Devastation" }, { "name": "Preservation" }] }]
+};
+
+var classCounter = 0;
+
+//manages control panel
+$(document).ready(function () {
+    var classHTML = "";
+
+    $.each(classes.classList, function (i, elementText) {
+        classHTML += "<option value ='class" + classCounter + "'>" + elementText.class + "</option>";
+        var subclassHTML = "<select name='" + elementText.class + "' id = 'class" + classCounter + "'>";
+        $.each(elementText.subclass, function (j, subElementText) {
+            subclassHTML += "<option value='" + subElementText.name + "'>" + subElementText.name + "</option>";
+        });
+        subclassHTML += "</select>"
+        $("#subclassDropdown").append(subclassHTML);
+        if (classCounter != 0) {
+            var id = "class" + classCounter;
+            var e = document.getElementById(id);
+            e.style.display = "none"
+        }
+        classCounter++;
+
+
+    });
+
+    slotHTML = "<form>";
+    //Slot Table
+    for (var i = 0; i < slots.length; i++) {
+        var slotName = slots[i];
+        slotName = slotName.charAt(0).toUpperCase() + slotName.slice(1);
+        slotHTML += "<input type='checkbox' id='stat" + i + "' value='" + slots[i] + "' name ='slot'>" +
+            "<label for ='stat" + i + "'>" + slotName + "</label><br>";
+    }
+    slotHTML += "<input type='submit' value='Filter Slots'>";
+    
+    $('#statPanel').append(slotHTML);
+    $("#classDropdown").append(classHTML);
+});
+
+//manages dynamic dropdown box
+window.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("classDropdown").addEventListener("change", function () {
+        var e = document.getElementById("classDropdown");
+        var value = e.value;
+
+
+
+        for (var i = 0; i < classCounter; i++) {
+            var id = "class" + i;
+            //console.log(id);
+            var element = document.getElementById(id);
+
+            if (value === id) {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+        }
+    })
+});
+
+let checkboxes = $("input[type=checkbox][name=slot]")
+let enabledSettings = [];
+
+checkboxes.change(function() {
+
+    //hide all relevent elements before reshowing them
+    for(var i in slots){
+        var allElements = document.getElementsByClassName(id);
+        for(var j in allElements){
+            j.style.block="none";
+        }
+    }
+
+    //reshow elements
+    enabledSettings = checkboxes
+      .filter(":checked") // Filter out unchecked boxes.
+      .map(function() { // Extract values using jQuery map.
+        var id = this.value;
+        var elements = document.getElementsByClassName(id);
+        for(var i in elements){
+            i.style.block = "block";
+        }
+      }) 
+  });
